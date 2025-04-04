6686b9d053e1441dee07b16f30a7d5c880516e4b
SIDDHARTHA UPPUNUTI <you@example.com>
2025-04-04 22:14:46 +0530

    Updated requirements.txt and other changes

--- base/models.py
+++ base/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Job(models.Model):
+    company = models.ForeignKey( on_delete=models.CASCADE)
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    required_skills = models.TextField()
+    posted_at = models.DateTimeField(auto_now_add=True)
